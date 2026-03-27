29557522e59292f0d2871fdef9d7bed7ef54539f
Ynda Jas <yndajas@gmail.com>
2026-03-27 18:56:55 +0000

    Remove unused yield from sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,7 +10,6 @@ class Users::SessionsController < Devise::SessionsController
       # Duplicates default Devise::SessionsController#create logic
       set_flash_message!(:notice, :signed_in)
       sign_in(resource_name, resource)
-      yield resource if block_given?
       respond_with resource, location: after_sign_in_path_for(resource)
     end
   end
