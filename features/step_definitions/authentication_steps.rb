988303c9cb8d7e09bf7538fd78104397f8f57cce
Ynda Jas <yndajas@gmail.com>
2026-03-27 19:18:08 +0000

    Organise shared Cucumber steps

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -2,19 +2,6 @@ Given "a user exists with email {string} and password {string}" do |email, passw
   User.create!(email:, password:)
 end
 
-When "I follow {string}" do |link|
-  click_link link
-end
-
-When "I fill in {string} with {string}" do |field, value|
-  fill_in field, with: value
-end
-
-When "I press {string}" do |button|
-  click_button button
-end
-
-
 Given "the user has not set up 2FA" do
   # This is the default state
   user = User.last
--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -1,3 +1,19 @@
+Given "I am on the home page" do
+  visit root_path
+end
+
+When "I follow {string}" do |link|
+  click_link link
+end
+
+When "I fill in {string} with {string}" do |field, value|
+  fill_in field, with: value
+end
+
+When "I press {string}" do |button|
+  click_button button
+end
+
 Then "I should see {string}" do |content|
   expect(page).to have_content(content)
 end
