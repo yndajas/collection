cee262401a5080baf36f170cb428f920c606a382
Ynda Jas <yndajas@gmail.com>
2026-03-27 11:56:23 +0000

    Update 'local' and real CI steps
    These were both missing the test suites and the former was missing
    the Markdown linting

--- config/ci.rb
+++ config/ci.rb
@@ -4,16 +4,11 @@ CI.run do
   step "Setup", "bin/setup --skip-server"
 
   step "Style: Ruby", "bin/rubocop"
+  step "Style: Markdown", "npm run lint:md"
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
 
-
-  # Optional: set a green GitHub commit status to unblock PR merge.
-  # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
-  # if success?
-  #   step "Signoff: All systems go. Ready for merge and deploy.", "gh signoff"
-  # else
-  #   failure "Signoff: CI failed. Do not merge or deploy.", "Fix the issues and try again."
-  # end
+  step "Tests: RSpec", "bundle exec rspec"
+  step "Tests: Cucumber", "bundle exec cucumber"
 end
